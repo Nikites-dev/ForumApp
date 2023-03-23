c0c44580e927bede5baecdbeb34a1b0455b55e34
Петров Никита <Petovn.xz@mail.ru>
2023-03-23 10:48:54 +0300

    add infinite scrollview in MainView page

--- forum_app/lib/pages/main_view.dart
+++ forum_app/lib/pages/main_view.dart
@@ -2,11 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'package:flutter/material.dart';
+import 'package:infinite_listview/infinite_listview.dart';
+
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text('data'),);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.17,
+            child: InfiniteListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    height: MediaQuery.of(context).size.width * 0.35,
+                    child: Card(
+                      child: ListTile(
+                        title: const Text('Horizontal'),
+                        subtitle: Text('Breaking news!!!$index'),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.45,
+            child: InfiniteListView.builder(itemBuilder: (context, index) {
+              return Center(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.7 * 0.25,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Card(
+                    child: ListTile(
+                      title: const Text('Vertical'),
+                      subtitle: Text('Листаюсь вертикально $index'),
+                    ),
+                  ),
+                ),
+              );
+            }),
+          )
+        ],
+      ),
+    );
   }
 }
