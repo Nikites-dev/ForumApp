135136c35d988693efdb1a37349e86bf3f4b61f0
Rinat050 <safiullin.rinat.05@gmail.com>
2023-05-18 20:13:46 +0300

    change post user info loading

--- forum_app/lib/widgets/post_user_info_widget.dart
+++ forum_app/lib/widgets/post_user_info_widget.dart
@@ -20,6 +20,7 @@ class PostUserInfoWidget extends StatefulWidget {
 class _PostUserInfoWidgetState extends State<PostUserInfoWidget> {
   final AuthServices _authServices = AuthServices();
   final DateFormat _dateFormatter = DateFormat('dd.MM.y HH:mm');
+  Future cacheUserInfoFuture = Future(() => null);
   
   Future<void> loadUserInfo() async
   {
@@ -28,51 +29,64 @@ class _PostUserInfoWidgetState extends State<PostUserInfoWidget> {
     widget.userName = info.username;
   }
 
-  Future<void> cacheUserInfo() async
+  Future<bool> cacheUserInfo() async
   {
     await _authServices.cacheInfo(widget.post.username!);
+
+    return true;
   }
 
+  @override
+  void initState() {
+    super.initState();
+    cacheUserInfoFuture = cacheUserInfo();
+  }
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         FutureBuilder(
-          future: cacheUserInfo(),
+          future: cacheUserInfoFuture,
           builder: (context, snapshot) {
-            return Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AuthServices.uniqueUsers[widget.post.username]!.userImg != 'null' ? CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        AuthServices.uniqueUsers[widget.post.username]!.userImg,),
-                  )
-                  : CircleAvatar(
-                    child: Text(AuthServices.uniqueUsers[widget.post.username]!.username[0], 
-                      style: const TextStyle(fontSize: 18,color: Colors.white),
+            if (snapshot.data != null && snapshot.data == true)
+            {
+              return Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AuthServices.uniqueUsers[widget.post.username]!.userImg != 'null' ? CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          AuthServices.uniqueUsers[widget.post.username]!.userImg,),
+                    )
+                    : CircleAvatar(
+                      child: Text(AuthServices.uniqueUsers[widget.post.username]!.username != 'null' 
+                        ? AuthServices.uniqueUsers[widget.post.username]!.username[0]
+                        : '',
+                        style: const TextStyle(fontSize: 18,color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: AuthServices.uniqueUsers[widget.post.username]!.username != 'null' ? [
-                    Padding(
-                          padding: const EdgeInsets.all(1.0),
-                          child: Text(AuthServices.uniqueUsers[widget.post.username]!.username),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: widget.post.createPost == null 
-                      ? const Text("не указано") 
-                      : Text(_dateFormatter.format(widget.post.createPost!), style: const TextStyle(fontSize: 12),),
-                    ),
-                  ] 
-                  : [LoadingAnimationWidget.waveDots(color: Colors.cyan, size: 10),]
-                ),
-              ],
-            );
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: AuthServices.uniqueUsers[widget.post.username]!.username != 'null' ? [
+                      Padding(
+                            padding: const EdgeInsets.all(1.0),
+                            child: Text(AuthServices.uniqueUsers[widget.post.username]!.username),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: widget.post.createPost == null 
+                        ? const Text("не указано") 
+                        : Text(_dateFormatter.format(widget.post.createPost!), style: const TextStyle(fontSize: 12),),
+                      ),
+                    ] 
+                    : [LoadingAnimationWidget.waveDots(color: Colors.cyan, size: 10),]
+                  ),
+                ],
+              );
+            }
+            return const Padding(padding: EdgeInsets.all(0.0));
           } 
         ),
         Row(
